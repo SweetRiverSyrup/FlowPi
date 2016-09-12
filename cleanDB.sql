5c20b8b2aa05df87e483e5073fc6b4b313ce7d24
Ian Fabs <ianfabs@outlook.com>
2016-09-12 19:47:05 +0000

    Changes to database structure

--- cleanDB.sql
+++ cleanDB.sql
@@ -16,229 +16,252 @@ USE `pi`;
 
 
 -- Dumping structure for table pi.biologicalsampling
+DROP TABLE IF EXISTS `biologicalsampling`;
 CREATE TABLE IF NOT EXISTS `biologicalsampling` (
-  `netLength` float(4,3) DEFAULT NULL,
-  `netWidth` float(4,3) DEFAULT NULL,
-  `meshSize` float(4,3) DEFAULT NULL,
+  `id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `class_uuid` varchar(99) DEFAULT NULL,
   `netStudents` varchar(255) DEFAULT NULL,
-  `methodsTableId` int(11) DEFAULT NULL,
-  `collections` int(11) DEFAULT NULL,
-  `totalIndividuals` int(11) DEFAULT NULL,
-  `surveyId` int(11) DEFAULT NULL,
-  `description` text
+  `methodsTableId` varchar(50) DEFAULT NULL,
+  `collections` varchar(50) DEFAULT NULL,
+  `totalIndividuals` varchar(50) DEFAULT NULL,
+  `surveyId` varchar(50) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table pi.biologicalsampling: ~0 rows (approximately)
-/*!40000 ALTER TABLE `biologicalsampling` DISABLE KEYS */;
-/*!40000 ALTER TABLE `biologicalsampling` ENABLE KEYS */;
+-- Data exporting was unselected.
 
 
 -- Dumping structure for table pi.chemicalanalysis
+DROP TABLE IF EXISTS `chemicalanalysis`;
 CREATE TABLE IF NOT EXISTS `chemicalanalysis` (
+  `id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `class_uuid` bigint(255) NOT NULL DEFAULT '0',
   `waterTempId` int(11) DEFAULT NULL,
-  `averageTempC` float(2,2) DEFAULT NULL,
-  `averageTempF` float(3,2) DEFAULT NULL,
-  `oxygenId` int(11) DEFAULT NULL,
+  `averageTempC` varchar(50) DEFAULT NULL,
+  `averageTempF` varchar(50) DEFAULT NULL,
+  `oxygenId` varchar(50) DEFAULT NULL,
   `pHLocation` varchar(255) DEFAULT NULL,
-  `pHTime` time DEFAULT NULL,
-  `reading1` float(1,1) DEFAULT NULL,
-  `reading2` float(1,1) DEFAULT NULL,
-  `reading3` float(1,1) DEFAULT NULL,
-  `average` float(5,5) DEFAULT NULL,
+  `pHTime` varchar(50) DEFAULT NULL,
+  `reading1` varchar(50) DEFAULT NULL,
+  `reading2` varchar(50) DEFAULT NULL,
+  `reading3` varchar(50) DEFAULT NULL,
+  `average` varchar(50) DEFAULT NULL,
   `optLocation` varchar(255) DEFAULT NULL,
-  `optTime` time DEFAULT NULL,
-  `optReading1` float(1,1) DEFAULT NULL,
-  `optReading2` float(1,1) DEFAULT NULL,
-  `optReading3` float(1,1) DEFAULT NULL,
-  `optAverage` float(5,5) DEFAULT NULL,
+  `optTime` varchar(50) DEFAULT NULL,
+  `optReading1` varchar(50) DEFAULT NULL,
+  `optReading2` varchar(50) DEFAULT NULL,
+  `optReading3` varchar(50) DEFAULT NULL,
+  `optAverage` varchar(50) DEFAULT NULL,
   `salinityLocation` varchar(255) DEFAULT NULL,
-  `salinityTime` time DEFAULT NULL,
-  `salinityReading1` float(4,3) DEFAULT NULL,
-  `salinityReading2` float(4,3) DEFAULT NULL,
-  `salinityReading3` float(4,3) DEFAULT NULL,
-  `salinityAverage` float(5,5) DEFAULT NULL,
+  `salinityTime` varchar(50) DEFAULT NULL,
+  `salinityReading1` varchar(50) DEFAULT NULL,
+  `salinityReading2` varchar(50) DEFAULT NULL,
+  `salinityReading3` varchar(50) DEFAULT NULL,
+  `salinityAverage` varchar(50) DEFAULT NULL,
   `turbiditySecchiLocation` varchar(255) DEFAULT NULL,
-  `turbiditySecchiTime` time DEFAULT NULL,
-  `turbiditySecchiReading1` float(4,3) DEFAULT NULL,
-  `turbiditySecchiReading2` float(4,3) DEFAULT NULL,
-  `turbiditySecchiReading3` float(4,3) DEFAULT NULL,
-  `turbiditySecchiAverage` float(5,5) DEFAULT NULL,
+  `turbiditySecchiTime` varchar(50) DEFAULT NULL,
+  `turbiditySecchiReading1` varchar(50) DEFAULT NULL,
+  `turbiditySecchiReading2` varchar(50) DEFAULT NULL,
+  `turbiditySecchiReading3` varchar(50) DEFAULT NULL,
+  `turbiditySecchiAverage` varchar(50) DEFAULT NULL,
   `turbidityShortLocation` varchar(255) DEFAULT NULL,
-  `turbidityShortTime` time DEFAULT NULL,
-  `turbidityShortReading1` float(4,3) DEFAULT NULL,
-  `turbidityShortReading2` float(4,3) DEFAULT NULL,
-  `turbidityShortReading3` float(4,3) DEFAULT NULL,
-  `turbidityShortAverage` float(5,5) DEFAULT NULL,
+  `turbidityShortTime` varchar(50) DEFAULT NULL,
+  `turbidityShortReading1` varchar(50) DEFAULT NULL,
+  `turbidityShortReading2` varchar(50) DEFAULT NULL,
+  `turbidityShortReading3` varchar(50) DEFAULT NULL,
+  `turbidityShortAverage` varchar(50) DEFAULT NULL,
   `turbidityLongLocation` varchar(255) DEFAULT NULL,
-  `turbidityLongTime` time DEFAULT NULL,
-  `turbidityLongReading1` float(4,3) DEFAULT NULL,
-  `turbidityLongReading2` float(4,3) DEFAULT NULL,
-  `turbidityLongReading3` float(4,3) DEFAULT NULL,
-  `turbidityLongAverage` float(5,5) DEFAULT NULL,
+  `turbidityLongTime` varchar(50) DEFAULT NULL,
+  `turbidityLongReading1` varchar(50) DEFAULT NULL,
+  `turbidityLongReading2` varchar(50) DEFAULT NULL,
+  `turbidityLongReading3` varchar(50) DEFAULT NULL,
+  `turbidityLongAverage` varchar(50) DEFAULT NULL,
   `turbidityTurbidimeterLocation` varchar(255) DEFAULT NULL,
-  `turbidityTurbidimeterTime` time DEFAULT NULL,
-  `turbidityTurbidimeterReading1` float(4,3) DEFAULT NULL,
-  `turbidityTurbidimeterReading2` float(4,3) DEFAULT NULL,
-  `turbidityTurbidimeterReading3` float(4,3) DEFAULT NULL,
-  `turbidityTurbidimeterAverage` float(5,5) DEFAULT NULL
+  `turbidityTurbidimeterTime` varchar(50) DEFAULT NULL,
+  `turbidityTurbidimeterReading1` varchar(50) DEFAULT NULL,
+  `turbidityTurbidimeterReading2` varchar(50) DEFAULT NULL,
+  `turbidityTurbidimeterReading3` varchar(50) DEFAULT NULL,
+  `turbidityTurbidimeterAverage` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table pi.chemicalanalysis: ~0 rows (approximately)
-/*!40000 ALTER TABLE `chemicalanalysis` DISABLE KEYS */;
-/*!40000 ALTER TABLE `chemicalanalysis` ENABLE KEYS */;
+-- Data exporting was unselected.
 
 
 -- Dumping structure for table pi.config
+DROP TABLE IF EXISTS `config`;
 CREATE TABLE IF NOT EXISTS `config` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_uuid` varchar(99) DEFAULT NULL,
   `river` varchar(69) DEFAULT NULL,
   `location` varchar(69) DEFAULT NULL,
   `site` varchar(69) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table pi.config: ~1 rows (approximately)
-/*!40000 ALTER TABLE `config` DISABLE KEYS */;
-INSERT INTO `config` (`id`, `river`, `location`, `site`) VALUES
-	(1, 'AARiver', 'h', 'l6');
-/*!40000 ALTER TABLE `config` ENABLE KEYS */;
+-- Data exporting was unselected.
 
 
 -- Dumping structure for table pi.documentation
+DROP TABLE IF EXISTS `documentation`;
 CREATE TABLE IF NOT EXISTS `documentation` (
+  `id` bigint(255) NOT NULL AUTO_INCREMENT,
   `photos` longblob,
   `description` text,
-  `teamsId` int(11) DEFAULT NULL,
+  `teamsId` varchar(50) DEFAULT NULL,
   `questions` text,
-  `groupPhoto` longblob
+  `groupPhoto` longblob,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table pi.documentation: ~0 rows (approximately)
-/*!40000 ALTER TABLE `documentation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `documentation` ENABLE KEYS */;
+-- Data exporting was unselected.
 
 
 -- Dumping structure for table pi.methods
+DROP TABLE IF EXISTS `methods`;
 CREATE TABLE IF NOT EXISTS `methods` (
-  `id` int(11) DEFAULT NULL,
+  `id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `key` bigint(255) DEFAULT NULL,
   `methodType` varchar(255) DEFAULT NULL,
-  `netDistance` float(4,3) DEFAULT NULL,
-  `speciesTableId` int(11) DEFAULT NULL,
-  `fishTotal` int(11) DEFAULT NULL,
-  `comments` varchar(255) DEFAULT NULL
+  `netLength` varchar(50) DEFAULT NULL,
+  `netWidth` varchar(50) DEFAULT NULL,
+  `netMeshSize` varchar(50) DEFAULT NULL,
+  `pullLength` varchar(50) DEFAULT NULL,
+  `speciesTableId` varchar(50) DEFAULT NULL,
+  `comments` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table pi.methods: ~0 rows (approximately)
-/*!40000 ALTER TABLE `methods` DISABLE KEYS */;
-/*!40000 ALTER TABLE `methods` ENABLE KEYS */;
+-- Data exporting was unselected.
 
 
 -- Dumping structure for table pi.physical_data
+DROP TABLE IF EXISTS `physical_data`;
 CREATE TABLE IF NOT EXISTS `physical_data` (
-  `tideChangeAtTime0` float(4,3) DEFAULT NULL,
-  `tideIsRFUAtTime0` enum('0','1','2') DEFAULT NULL,
-  `tideChangeAtTime10` float(4,3) DEFAULT NULL,
-  `tideIsRFUAtTime10` enum('0','1','2') DEFAULT NULL,
-  `tideChangeAtTime20` float(4,3) DEFAULT NULL,
-  `tideIsRFUAtTime20` enum('0','1','2') DEFAULT NULL,
-  `tideChangeAtTime30` float(4,3) DEFAULT NULL,
-  `tideIsRFUAtTime30` enum('0','1','2') DEFAULT NULL,
-  `totalTideChange` float(4,3) DEFAULT NULL,
+  `id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `tideChangeAtTime10` varchar(50) DEFAULT NULL,
+  `tideIsRFUAtTime10` varchar(15) DEFAULT NULL,
+  `tideChangeAtTime20` varchar(50) DEFAULT NULL,
+  `tideIsRFUAtTime20` varchar(15) DEFAULT NULL,
+  `tideChangeAtTime30` varchar(50) DEFAULT NULL,
+  `tideIsRFUAtTime30` varchar(15) DEFAULT NULL,
+  `totalTideChange` varchar(50) DEFAULT NULL,
   `waterCurrentMeasurementObject` varchar(20) DEFAULT NULL,
-  `waterCurrentTable` int(11) DEFAULT NULL,
-  `tempTableId` int(11) DEFAULT NULL,
+  `currentSpeedCMpSAvg` varchar(50) DEFAULT NULL,
+  `currentSpeedKntsAvg` varchar(50) DEFAULT NULL,
   `time1` varchar(32) DEFAULT NULL,
-  `tempF1` float(4,3) DEFAULT NULL,
-  `tempC1` float(4,3) DEFAULT NULL,
-  `cloudCoverage` enum('0','1','2','3') DEFAULT NULL,
+  `tempF1` varchar(50) DEFAULT NULL,
+  `tempC1` varchar(50) DEFAULT NULL,
+  `time2` varchar(32) DEFAULT NULL,
+  `tempF2` varchar(50) DEFAULT NULL,
+  `tempC2` varchar(50) DEFAULT NULL,
+  `time3` varchar(32) DEFAULT NULL,
+  `tempF3` varchar(50) DEFAULT NULL,
+  `tempC3` varchar(50) DEFAULT NULL,
+  `cloudCover` varchar(20) DEFAULT NULL,
   `windDirection` varchar(20) DEFAULT NULL,
-  `windSpeedMph` float(4,3) DEFAULT NULL,
-  `windSpeedKnots` float(4,3) DEFAULT NULL,
-  `monthYear` varchar(25) DEFAULT NULL,
-  `beaufortDescription` varchar(255) DEFAULT NULL
+  `windSpeedMph` varchar(50) DEFAULT NULL,
+  `windSpeedKnots` varchar(50) DEFAULT NULL,
+  `windBeaufortScale` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table pi.physical_data: ~0 rows (approximately)
-/*!40000 ALTER TABLE `physical_data` DISABLE KEYS */;
-/*!40000 ALTER TABLE `physical_data` ENABLE KEYS */;
+-- Data exporting was unselected.
 
 
 -- Dumping structure for table pi.site_description
+DROP TABLE IF EXISTS `site_description`;
 CREATE TABLE IF NOT EXISTS `site_description` (
-  `latitude` float(3,2) DEFAULT NULL,
-  `longitude` float(3,2) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `riverDescription` enum('0','1','2','3','4') DEFAULT NULL,
-  `vegetationBottom` enum('0','1','2','3') DEFAULT NULL,
-  `vegetationSurface` enum('0','1','2','3') DEFAULT NULL,
+  `id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `latitude` varchar(50) DEFAULT NULL,
+  `longitude` varchar(50) DEFAULT NULL,
+  `g2CheckboxArray` varchar(255) DEFAULT NULL,
+  `riverBayIs` varchar(50) DEFAULT NULL,
+  `vegBottomPercent` varchar(50) DEFAULT NULL,
+  `vegSurfacePercent` varchar(50) DEFAULT NULL,
   `sedimentSample` longblob,
   `sedimentSketch` longblob,
-  `lengthSedimentCore` float(4,3) DEFAULT NULL,
-  `lengthOxidizedCore` float(4,3) DEFAULT NULL,
-  `lengthAnoxicCore` float(4,3) DEFAULT NULL
+  `lengthSedimentCore` varchar(50) DEFAULT NULL,
+  `lengthOxidizedCore` varchar(50) DEFAULT NULL,
+  `lengthAnoxicCore` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table pi.site_description: ~0 rows (approximately)
-/*!40000 ALTER TABLE `site_description` DISABLE KEYS */;
-/*!40000 ALTER TABLE `site_description` ENABLE KEYS */;
+-- Data exporting was unselected.
 
 
 -- Dumping structure for table pi.speciesfound
+DROP TABLE IF EXISTS `speciesfound`;
 CREATE TABLE IF NOT EXISTS `speciesfound` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `speciesName` varchar(255) DEFAULT NULL,
-  `totalNumber` int(11) DEFAULT NULL,
-  `largestSize` float(4,3) DEFAULT NULL,
-  `units` varchar(32) DEFAULT NULL
+  `totalNumber` varchar(50) DEFAULT NULL,
+  `largestSize` varchar(50) DEFAULT NULL,
+  `units` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table pi.speciesfound: ~0 rows (approximately)
-/*!40000 ALTER TABLE `speciesfound` DISABLE KEYS */;
-/*!40000 ALTER TABLE `speciesfound` ENABLE KEYS */;
+-- Data exporting was unselected.
 
 
 -- Dumping structure for table pi.speciestable2
+DROP TABLE IF EXISTS `speciestable2`;
 CREATE TABLE IF NOT EXISTS `speciestable2` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `key` bigint(255) DEFAULT NULL,
   `location` varchar(255) DEFAULT NULL,
   `species` varchar(255) DEFAULT NULL,
-  `count` int(11) DEFAULT NULL
+  `count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table pi.speciestable2: ~0 rows (approximately)
-/*!40000 ALTER TABLE `speciestable2` DISABLE KEYS */;
-/*!40000 ALTER TABLE `speciestable2` ENABLE KEYS */;
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table pi.students
+DROP TABLE IF EXISTS `students`;
+CREATE TABLE IF NOT EXISTS `students` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_uuid` varchar(99) NOT NULL,
+  `student_uuid` varchar(99) NOT NULL,
+  `student_name` varchar(99) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
 
 
 -- Dumping structure for table pi.teammembers
+DROP TABLE IF EXISTS `teammembers`;
 CREATE TABLE IF NOT EXISTS `teammembers` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `group1` varchar(255) DEFAULT NULL,
   `group2` varchar(255) DEFAULT NULL,
   `group3` varchar(255) DEFAULT NULL,
-  `group4` varchar(255) DEFAULT NULL
+  `group4` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table pi.teammembers: ~0 rows (approximately)
-/*!40000 ALTER TABLE `teammembers` DISABLE KEYS */;
-/*!40000 ALTER TABLE `teammembers` ENABLE KEYS */;
+-- Data exporting was unselected.
 
 
 -- Dumping structure for table pi.watercurrenttable
+DROP TABLE IF EXISTS `watercurrenttable`;
 CREATE TABLE IF NOT EXISTS `watercurrenttable` (
-  `location` varchar(255) DEFAULT NULL,
-  `startTime` time DEFAULT NULL,
-  `stopTime` time DEFAULT NULL,
-  `distanceTraveled` float(4,3) DEFAULT NULL,
+  `id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `key` bigint(255) NOT NULL DEFAULT '0',
+  `startTime` varchar(32) DEFAULT NULL,
+  `stopTime` varchar(32) DEFAULT NULL,
+  `distanceTraveled` varchar(255) DEFAULT NULL,
   `direction` varchar(20) DEFAULT NULL,
-  `currentSpeedCmPerSec` float(4,3) DEFAULT NULL,
-  `currentSpeedKnots` float(4,3) DEFAULT NULL,
-  `ebbFlowSlack` enum('0','1','2') DEFAULT NULL
+  `currentSpeedCmPerSec` varchar(50) DEFAULT NULL,
+  `currentSpeedKnots` varchar(50) DEFAULT NULL,
+  `ebbFlowSlack` varchar(225) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table pi.watercurrenttable: ~0 rows (approximately)
-/*!40000 ALTER TABLE `watercurrenttable` DISABLE KEYS */;
-/*!40000 ALTER TABLE `watercurrenttable` ENABLE KEYS */;
+-- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
